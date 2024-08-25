0d81bfaca153e2f81ed0e0a80a95bf42b82dda2b
Prado Matias Santiago <matias14prado@gmail.com>
2024-08-25 14:03:05 -0300

    Clase cadete casi finalizada, falta probar en la implementacion los metodos y la carga de sus campos

--- cadete.cs
+++ cadete.cs
@@ -9,5 +9,76 @@ namespace espacioCadetes
         private int telefono;
         private List<Pedidos> listaPedidos;
 
+        public Cadete(int id, string nombre, string direccion, int telefono)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            listaPedidos = new List<Pedidos>();
+        }
+
+        public void MostrarPedidos()
+        {
+            System.Console.WriteLine("----------LISTA PEDIDOS ----------");
+            foreach (var pedido in listaPedidos)
+            {
+                System.Console.WriteLine($"Pedido Nro: {pedido.NroPedido}, Estado: {pedido.Estado}");
+            }
+        }
+
+        public void MostrarID()
+        {
+            System.Console.WriteLine($"El ID del cadete {nombre} es: {id}");
+        }
+
+        public void MostrarDatos()
+        {
+            System.Console.WriteLine($"Nombre: {nombre}\n Dirección: {direccion}\nTeléfono: {telefono}");
+        }
+
+        public void EntregarPedido()
+        {
+            int indicePedido = 0;
+            int nroPedidoEntregar;
+            System.Console.WriteLine("Ingrese el numero de pedido para entregar:");
+            while(int.TryParse(Console.ReadLine(), out nroPedidoEntregar))
+            {
+                Console.WriteLine("Ingrese un numero valido de pedido:");
+            }
+            foreach(Pedidos pedido in listaPedidos)
+            {
+                if(pedido.NroPedido == nroPedidoEntregar)
+                {
+                    break;
+                }
+                indicePedido++;
+            }
+
+            listaPedidos[indicePedido].Estado = Estado.Entregado;
+        }
+
+        public void CancelarPedido()
+        {
+            int indicePedido = 0;
+            int nroPedidoEntregar;
+            System.Console.WriteLine("Ingrese el numero de pedido para cancelar:");
+            while(int.TryParse(Console.ReadLine(), out nroPedidoEntregar))
+            {
+                Console.WriteLine("Ingrese un numero valido de pedido:");
+            }
+            foreach(Pedidos pedido in listaPedidos)
+            {
+                if(pedido.NroPedido == nroPedidoEntregar)
+                {
+                    break;
+                }
+                indicePedido++;
+            }
+
+            listaPedidos[indicePedido].Estado = Estado.Cancelado;
+        }
+
     }
+
 }
