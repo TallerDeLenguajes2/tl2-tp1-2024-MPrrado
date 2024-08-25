6ea124bb4716713d3e15472b4351fa234dde60ad
Prado Matias Santiago <matias14prado@gmail.com>
2024-08-25 18:19:16 -0300

    Movimos unos de los metodos de la clase "Cadete" hacia la clase "Cadeteria", ademas modificamos la presentacion de algunos datos de los metodos de Cadeteria

--- cadete.cs
+++ cadete.cs
@@ -22,60 +22,20 @@ namespace espacioCadetes
 
         public void MostrarPedidos()
         {
-            System.Console.WriteLine("----------LISTA PEDIDOS ----------");
             foreach (var pedido in listaPedidos)
             {
-                System.Console.WriteLine($"Pedido Nro: {pedido.NroPedido}, Estado: {pedido.Estado}");
+                System.Console.WriteLine($"-------Pedido Nro: {pedido.NroPedido}, Estado: {pedido.Estado}");
             }
         }
 
         public void MostrarID()
         {
-            System.Console.WriteLine($"El ID del cadete {nombre} es: {id}");
+            System.Console.WriteLine($"El ID del cadete es: {id}");
         }
 
         public void MostrarDatos()
         {
-            System.Console.WriteLine($"Nombre: {nombre}\n Dirección: {direccion}\nTeléfono: {telefono}");
-        }
-
-        public void CambiarEstadoPedido(List<Pedidos> listaPedidos)
-        {
-            int nroPedido;
-            while(!int.TryParse(Console.ReadLine(), out nroPedido) || nroPedido > listaPedidos.Count || nroPedido < 1)
-            {
-                System.Console.WriteLine("Error ingrese un numero de pedido valido");
-            }
-
-            foreach (var pedido in listaPedidos)
-            {
-                if(pedido.NroPedido == nroPedido)
-                {
-                    System.Console.WriteLine("Seleccione que desea hacer con este pedido: ");
-                    System.Console.WriteLine();
-                    int opcion = 0;
-                    while(opcion <1 || opcion > 2)
-                    {
-                        System.Console.WriteLine("[1] ENTREGADO");
-                        System.Console.WriteLine("[2] CANCELADO");
-                        int.TryParse(Console.ReadLine(), out opcion);
-                        if(opcion <1 || opcion > 2)
-                        {
-                            System.Console.WriteLine("ERROR, ingrese una opcion válida");
-                        }
-                    }
-
-                    switch(opcion)
-                    {
-                        case 1:
-                            pedido.Estado = Estado.Entregado;
-                            break;
-                        case 2:
-                            pedido.Estado = Estado.Cancelado;
-                            break;
-                    }
-                }
-            }
+            System.Console.WriteLine($"Nombre: {nombre}\nDirección: {direccion}\nTeléfono: {telefono}");
         }
     
         public void AgregarPedido(Pedidos pedido)
--- cadeteria.cs
+++ cadeteria.cs
@@ -22,36 +22,100 @@ namespace espacioCadeteria
         {
             Random rnd = new Random();
             Pedidos pedido = new Pedidos(nroPedidoAlta, observaciones[rnd.Next(0, 10)]);
+            nroPedidoAlta++;
             return pedido;
         }
 
         public void AsignarPedido(Pedidos pedido)
         {
+            
             int nroCadete;
             //Aquí se selecciona un cadete y se le asigna el pedido
 
             MostrarCadetes();
 
-            System.Console.WriteLine("Seleccione el cadete para asignar el pedido");
+            System.Console.WriteLine("Seleccione el ID del cadete para asignar el pedido");
             while (!int.TryParse(Console.ReadLine(), out nroCadete) || nroCadete > listadoCadetes.Count)
             {
                 System.Console.WriteLine("ERROR, ingrese un numero valido");
             }
             var cadeteAsignado = listadoCadetes[nroCadete - 1];
             cadeteAsignado.AgregarPedido(pedido);
+            System.Console.WriteLine("ASGINACION CON EXITO!");
+            Thread.Sleep(500);
+            Console.Clear();
         }
 
-        public void ReasignarPedidoCadete(List<Pedidos> listaPedidos)
+        public void CambiarEstadoPedido(List<Pedidos> listaPedidos)
         {
-            System.Console.WriteLine("Los pedidos disponibles para reasignar son:");
+            int nroPedido;
+            System.Console.WriteLine("Los pedidos disponibles son:");
+            System.Console.WriteLine();
+            System.Console.WriteLine();
             foreach (var pedido in listaPedidos)
             {
                 if (pedido.Estado != Estado.Cancelado || pedido.Estado != Estado.Entregado)
                 {
                     pedido.mostrarPedido();
                 }
+                System.Console.WriteLine("---------------------------------------");
+            }
+            System.Console.WriteLine("Seleccione el numero de pedido que desea cambiar de estado");
+            while(!int.TryParse(Console.ReadLine(), out nroPedido) || nroPedido > listaPedidos.Count || nroPedido < 1)
+            {
+                System.Console.WriteLine("Error ingrese un numero de pedido valido");
+            }
+
+            foreach (var pedido in listaPedidos)
+            {
+                if(pedido.NroPedido == nroPedido)
+                {
+                    System.Console.WriteLine("Seleccione que desea hacer con este pedido: ");
+                    System.Console.WriteLine();
+                    int opcion = 0;
+                    while(opcion <1 || opcion > 2)
+                    {
+                        System.Console.WriteLine("[1] ENTREGADO");
+                        System.Console.WriteLine("[2] CANCELADO");
+                        int.TryParse(Console.ReadLine(), out opcion);
+                        if(opcion <1 || opcion > 2)
+                        {
+                            System.Console.WriteLine("ERROR, ingrese una opcion válida");
+                        }
+                    }
+
+                    switch(opcion)
+                    {
+                        case 1:
+                            pedido.Estado = Estado.Entregado;
+                            System.Console.WriteLine("PEDIDO ENTREGADO CON EXITO!");
+                            Thread.Sleep(500);
+                            break;
+                        case 2:
+                            pedido.Estado = Estado.Cancelado;
+                            System.Console.WriteLine("PEDIDO CANCELADO CON EXITO!");
+                            Thread.Sleep(500);
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void ReasignarPedidoCadete(List<Pedidos> listaPedidos)
+        {
+            System.Console.WriteLine("Los pedidos disponibles para reasignar son:");
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+            foreach (var pedido in listaPedidos)
+            {
+                if (pedido.Estado != Estado.Cancelado || pedido.Estado != Estado.Entregado)
+                {
+                    pedido.mostrarPedido();
+                }
+                System.Console.WriteLine("---------------------------------------");
             }
             int nroPedido;
+            System.Console.WriteLine("INGRESE EL NUMERO DE PEDIDO A REASIGNAR: ");
             while (!int.TryParse(Console.ReadLine(), out nroPedido) || nroPedido > listaPedidos.Count)
             {
                 System.Console.WriteLine("ERROR, ingrese un numero valido");
@@ -91,11 +155,12 @@ namespace espacioCadeteria
             {
                 cadete.MostrarID();
                 cadete.MostrarDatos();
-                System.Console.WriteLine("------------------------------------");
                 System.Console.WriteLine("pedidos del cadete:");
+                System.Console.WriteLine();
                 cadete.MostrarPedidos();
+                System.Console.WriteLine("------------------------------------");
+                System.Console.WriteLine();
             }
-            System.Console.WriteLine("------------------------------------");
         }
     }
 }
