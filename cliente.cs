7544ee5c3ea7f40be22c1743e1f299b6d424666f
Prado Matias Santiago <matias14prado@gmail.com>
2024-08-25 17:07:43 -0300

    modificacion de los tipos de datos que reciben los campos

--- cliente.cs
+++ cliente.cs
@@ -2,10 +2,10 @@ namespace Clientes
 {
     public class Cliente
     {
-        public string? nombre;
-        public string? direccion;
-        public string? refDireccion;
-        public int? telefono;
+        public string nombre;
+        public string direccion;
+        public string refDireccion;
+        public int telefono;
         public Cliente(string nombre, string direccion,string refDireccion,int telefono)
         {
             this.nombre = nombre;
